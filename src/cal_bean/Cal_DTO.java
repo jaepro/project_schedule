b565cafcebeb6d67adec1c55ff62798e3af583c9
jaepro <jaepro@naver.com>
2024-07-30 16:52:03 +0900

    DTO 수정

--- src/cal_bean/Cal_DTO.java
+++ src/cal_bean/Cal_DTO.java
@@ -1,5 +1,63 @@
 package cal_bean;
 
 public class Cal_DTO {
+	private String name, id, pwd, birth, content; //사용자 정보 변수
+	private int year, month, week, lastday;  //달력 만들 때 필요한 변수
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public int getMonth() {
+		return month;
+	}
+	public void setMonth(int month) {
+		this.month = month;
+	}
+	public int getWeek() {
+		return week;
+	}
+	public void setWeek(int week) {
+		this.week = week;
+	}
+	public int getLastday() {
+		return lastday;
+	}
+	public void setLastday(int lastday) {
+		this.lastday = lastday;
+	}
+	
+	
 
 }
