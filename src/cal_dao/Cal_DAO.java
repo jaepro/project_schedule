c4b47259f18a2c176c6109db3bc5a2302ec6ff7f
SEHYEON-YELLOW <kshyeon114@gmail.com>
2024-08-02 14:07:50 +0900

    Update Cal_DAO.java

--- src/cal_dao/Cal_DAO.java
+++ src/cal_dao/Cal_DAO.java
@@ -148,7 +148,7 @@ public class Cal_DAO{
 		int no = 0;
 		getConnection();
 		try {
-			pstmt = con.prepareStatement("insert into cal_member values(?,?,?,To_Date(?, 'yyMMdd'))");
+			pstmt = con.prepareStatement("insert into cal_member values(?,?,?,To_Date(?, 'yyyyMMdd'))");
 			
 			pstmt.setString(1, cal_DTO.getId());
 			pstmt.setString(2, cal_DTO.getPwd());
